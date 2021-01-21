ec8b6ca6e93fcdd9b198205f51c4453bff335a0c
Raphael Roullet <raroullet@gmail.com>
2021-01-21 17:33:17 +0000

    add hh-ethers import

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -10,7 +11,6 @@ task("accounts", "Prints the list of accounts", async () => {
   }
 });
 
-
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -21,11 +21,17 @@ module.exports = {
   defaultNetwork: "kovan",
   networks: {
     hardhat: {
-      accounts: [{privateKey:process.env.ACCOUNT_KEY,balance:"100000000000000000000"}]
+      accounts: [
+        {
+          privateKey: process.env.ACCOUNT_KEY,
+          balance: "100000000000000000000",
+        },
+      ],
     },
     kovan: {
       url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.ACCOUNT_KEY]
-    }
-  },  solidity: "0.6.12",
+      accounts: [process.env.ACCOUNT_KEY],
+    },
+  },
+  solidity: "0.6.12",
 };
