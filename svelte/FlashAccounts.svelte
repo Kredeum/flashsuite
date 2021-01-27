1a9b2ee65bb9950f886252f7b1b9e5519a479e81
Alain Papazoglou <alain@kredeum.com>
2021-01-27 15:55:24 +0100

    wip

--- svelte/FlashAccounts.svelte
+++ svelte/FlashAccounts.svelte
@@ -1,44 +1,109 @@
 <svelte:options tag="flashsuite-accounts" immutable={true} />
 
 <script>
-  import Dashboard from "./Dashboard.svelte";
   import { ethers } from "ethers";
+  import { onMount } from "svelte";
+  import { Dashboards } from "./stores.mjs";
+  import Dashboard from "./Dashboard.svelte";
+  import FlashAccounts from "../lib/contracts/FlashAccounts.mjs";
 
-  let ethersProvider;
-  let signer;
-  let network;
+  let dashboards = [];
+  $: addresses = [];
   let address;
-  let balance;
+  let signer = {};
+  let signerFrom, signerTo;
+  let network;
+  Dashboards.subscribe((value) => {
+    dashboards = value;
+  });
 
-  async function starting() {
-    if (window.ethereum) {
-      window.ethereum.enable();
-      ethersProvider = new ethers.providers.Web3Provider(window.ethereum);
-      network = (await ethersProvider.getNetwork()).name;
-      signer = ethersProvider.getSigner();    
-      address = await signer.getAddress();
-    } else {
-      console.log("Please install MetaMask!");
+  async function handleAccounts(accounts) {
+    address = accounts[0];
+    console.log("handleAccounts", accounts[0], addresses);
+    if (address) {
+      signer = new ethers.providers.Web3Provider(ethereum).getSigner();
+      if (addresses.indexOf(address) == -1) {
+        addresses = [...addresses, address];
+      }
     }
+    console.log("handleAccounts", accounts, addresses);
   }
-  starting();
+  async function handleChain(chainId) {
+    const networks = new Map([
+      [1, "mainnet"],
+      [3, "ropsten"],
+      [4, "rinkeby"],
+      [5, "goerli"],
+      [42, "kovan"],
+    ]);
+    network = networks.get(Number(chainId));
+    if (chainId != 42) {
+      alert(
+        "FlashAccount is in beta mode ! only available on Kovan\nPlease switch to the Kovan testnet"
+      );
+    }
+    console.log("handleChain", chainId, network);
+  }
 
-  flashLoan()
-  
+  onMount(async function () {
+    try {
+      handleChain(await ethereum.request({ method: "eth_chainId" }));
+      handleAccounts(await ethereum.request({ method: "eth_accounts" }));
+    } catch (e) {
+      alert("Please install MetaMask!", e);
+    }
+    await FlashAccounts.Init(true);
+  });
+
+  ethereum.on("connect", console.log);
+  ethereum.on("message", console.log);
+  ethereum.on("disconnect", console.error);
+  ethereum.on("chainChanged", handleChain);
+  ethereum.on("accountsChanged", handleAccounts);
+
+  async function step1() {
+    signerFrom = await signer.getAddress();
+    console.log(signerFrom);
+    console.log(dashboards[signerFrom]);
+    console.log(dashboards);
+    console.log( JSON.stringify(dashboards));
+   
+    await FlashAccounts.approveTransfers(dashboards[signerFrom], signer);
+  }
+  async function step2() {
+    signerTo = await signer.getAddress();
+    await FlashAccounts.approveLoans(dashboards[signerFrom], signer);
+  }
+  async function step3() {
+    await FlashAccounts.callFlashLoan(
+      dashboards[signerFrom],
+      signerFrom,
+      signerTo
+    );
+  }
 </script>
 
 <main>
-  <img src="logo.png" width ="600" alt="FlashSuite"/>
+  <img src="logo.png" width="600" alt="FlashSuite" />
+
+  <hr />
+  <p>
+    <button on:click={step1}>STEP 1 : Approve Tranfers</button>
+    <button on:click={step2}>STEP 2 : Approve Loan</button>
+    <button on:click={step3}>STEP 3 : Call FlashLoan</button>
+  </p>
+
   <table>
-    <tr><td class="cadre"><Dashboard user={"Alice"} /></td> </tr>
-    <tr><td class="cadre"><Dashboard user={"Bob"} /></td> </tr>
+    {#each addresses as address}
+      <tr><td class="cadre"><Dashboard user={address} /></td> </tr>
+    {/each}
   </table>
 
   <hr />
-  <h4>metamask user</h4>
+  <h4>metamask</h4>
   <small>
     network: {network}<br />
-    address: {address}<br />
+    account: {address}<br />
   </small>
 
   <hr />
@@ -49,14 +114,6 @@
     padding: 1em;
     margin: 0 auto;
   }
-  h1 {
-    color: purple;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-  
-
   table {
     width: 300px;
   }
