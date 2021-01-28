2cfab7f83eb04ac922fe75538772bae9f0183e7b
Alain Papazoglou <alain@kredeum.com>
2021-01-28 17:07:54 +0100

    add some graphql requests

---
+++ thegraph/aave2.mjs
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import fs from 'fs';
+
+async function graphQL(url, query) {
+  let response;
+  try { response = await axios.post(url, { query: query }); }
+  catch (error) { console.error(error); }
+  return response;
+}
+
+const queryFile = process.argv[2] || "flashloans.gql";
+const url = process.argv[3] || 'https://api.thegraph.com/subgraphs/name/aave/protocol-v2-kovan';
+const query = fs.readFileSync(queryFile, "utf8");
+
+graphQL(url, query).then((res) => {  console.log(JSON.stringify(res.data, null, "  "));  });
