1a559810f5986f0f085b91c9eeabb5981e7c5a39
Alain Papazoglou <alain@kredeum.com>
2021-02-06 13:17:59 +0100

    quick fixes

--- svelte/dashboard.svelte
+++ svelte/dashboard.svelte
@@ -17,24 +17,23 @@
   }
   function _hf(_healthFactor) {
     let ret;
-    if ((_healthFactor === "_") || (_healthFactor === "∞") ) {
+    if (_healthFactor === "_" || _healthFactor === "∞") {
       ret = _healthFactor;
     } else {
-      const hf = _bal(_healthFactor) 
+      const hf = _bal(_healthFactor);
       const bhf = BigNumber.from(_healthFactor);
       let warning = "";
-      if ( bhf.eq(0) ) {
+      if (bhf.eq(0)) {
         warning = "***";
-      } else if ( bhf.lt(BigNumber.from(10).pow(17).mul(8)) ) {
+      } else if (bhf.lt(BigNumber.from(10).pow(17).mul(8))) {
         warning = "**";
-      } else if ( bhf.lt(BigNumber.from(10).pow(17).mul(12)) ) {
-        ret = "*"
-      } 
+      } else if (bhf.lt(BigNumber.from(10).pow(17).mul(12))) {
+        ret = "*";
+      }
       ret = warning + hf + warning;
     }
     return ret;
   }
-
 
   let healthFactorAll = 0;
   let healthFactorChecked = 0;
@@ -60,9 +59,9 @@
         }
       }
       handleHealthFactor();
-    refresh++;
+      refresh++;
     }
-    console.log("getDashboard",_address, _force, "=>", $Dashboards[_address]);
+    console.log("getDashboard", _address, _force, "=>", $Dashboards[_address]);
     return $Dashboards[_address];
   }
   async function handleHealthFactor() {
@@ -148,155 +147,131 @@
     <p class="bottom">
       <button on:click={getDashboard(address, true)}>Refresh Dashboard</button>
     </p> -->
-    <div
-      id="OriginPosition"
-      class="fs-col-origin columnposition w-col w-col-6 w-col-stack w-col-small-small-stack"
-      style="min-height: 220px;"
-    >
-      <div class="columntitlebar reverse">
-        <h2 id="columnTitle">{name}</h2>
-        <ListBox bind:value={address} options={Object.keys($Dashboards)} />
-        <img
-          src="images/Network-Dot-Green.svg"
-          loading="lazy"
-          width="50"
-          alt=""
-          class="connectindicator"
-        />
-      </div>
+  <div id="OriginPosition" class="fs-col-origin columnposition w-col w-col-6 w-col-stack w-col-small-small-stack" style="min-height: 220px;">
+    <div class="columntitlebar reverse">
+      <h2 id="columnTitle">{name}</h2>
+      <ListBox bind:value={address} options={Object.keys($Dashboards)} />
+      <img src="images/Network-Dot-Green.svg" loading="lazy" width="50" alt="" class="connectindicator" />
+    </div>
 
-      {#await currentDashboard}
-        <p style="text-align: center;">loading</p>
-      {:then dashboard}
-        {#if dashboard}
-          <div
-            id="gridOrigin"
-            class="w-layout-grid gridorigin fs-grid-dashboard"
-          >
-            <h3 class="left">Your Deposits</h3>
-            <h3 class="right">Your Loans</h3>
-            {#if dashboard.tokens.length > 0}
-              <div class="fs-item-container">
-                {#each dashboard.tokens as item}
-                  {#if item.type == 0}
-                    <div
-                      class:checked={item.checked}
-                      class:fs-dashboard-item__origin={isOrigin}
-                      class="deposititem fs-deposit-item"
-                      on:click={() =>
-                        isOrigin && setChecked(item.symbol, !item.checked)}
-                      value={item.symbol}
-                      checked={item.checked}
-                    >
-                      <div class="tokendetails">
-                        <div id="platformAddressLogo" class="buttondisk">
-                          <img
-                            src="https://d3e54v103j8qbb.cloudfront.net/plugins/Basic/assets/placeholder.60f9b1840c.svg"
-                            loading="lazy"
-                            id="tokenLogoDep01ORG"
-                            alt=""
-                            class="placeholderimage"
-                          />
-                        </div>
-                        <div
-                          id="tokenSymbolDep01ORG"
-                          class="textlightmode label"
-                        >
-                          {item.symbol}
-                        </div>
+    {#await currentDashboard}
+      <p style="text-align: center;">loading</p>
+    {:then dashboard}
+      {#if dashboard}
+        <div id="gridOrigin" class="w-layout-grid gridorigin fs-grid-dashboard">
+          <h3 class="left">Your Deposits</h3>
+          <h3 class="right">Your Loans</h3>
+          {#if dashboard.tokens.length > 0}
+            <div class="fs-item-container">
+              {#each dashboard.tokens as item}
+                {#if item.type == 0}
+                  <div
+                    class:checked={item.checked}
+                    class:fs-dashboard-item__origin={name == "Origin"}
+                    class="deposititem fs-deposit-item"
+                    on:click={() => name == "Origin" && setChecked(item.symbol, !item.checked)}
+                    value={item.symbol}
+                    checked={item.checked}
+                  >
+                    <div class="tokendetails">
+                      <div id="platformAddressLogo" class="buttondisk">
+                        <img
+                          src="https://d3e54v103j8qbb.cloudfront.net/plugins/Basic/assets/placeholder.60f9b1840c.svg"
+                          loading="lazy"
+                          id="tokenLogoDep01ORG"
+                          alt=""
+                          class="placeholderimage"
+                        />
                       </div>
-                      <div class="readonlyfield">
-                        <div id="amountDep01ORG" class="textlightmode numbers">
-                          {_bal(item.amount, item.decimals)}
-                        </div>
+                      <div id="tokenSymbolDep01ORG" class="textlightmode label">
+                        {item.symbol}
                       </div>
-                      {#if isOrigin}
-                        <div class="fs-checkmark">
-                          {#if item.checked}V{:else}O{/if}
-                        </div>
-                      {/if}
                     </div>
-                  {/if}
-                {/each}
-              </div>
+                    <div class="readonlyfield">
+                      <div id="amountDep01ORG" class="textlightmode numbers">
+                        {_bal(item.amount, item.decimals)}
+                      </div>
+                    </div>
+                    {#if name == "Origin"}
+                      <div class="fs-checkmark">
+                        {#if item.checked}V{:else}O{/if}
+                      </div>
+                    {/if}
+                  </div>
+                {/if}
+              {/each}
+            </div>
 
-              <div class="fs-item-container">
-                {#each dashboard.tokens as item}
-                  {#if item.type > 0}
-                    <div
-                      class:checked={item.checked}
-                      class:fs-dashboard-item__origin={name === "Origin"}
-                      class="loanitem fs-dashboard-item  fs-loan-item"
-                      on:click={() =>
-                        isOrigin && setChecked(item.symbol, !item.checked)}
-                      value={item.symbol}
-                      checked={item.checked}
-                    >
-                      <div class="tokendetails reverse">
-                        <div
-                          id="platformAddressLogo"
-                          class="buttondisk reverse"
-                        >
-                          <img
-                            src="https://d3e54v103j8qbb.cloudfront.net/plugins/Basic/assets/placeholder.60f9b1840c.svg"
-                            loading="lazy"
-                            id="tokenLogoLoan01ORG"
-                            alt=""
-                            class="placeholderimage"
-                          />
-                        </div>
-                        <div id="tokenSymbolLoan01" class="textlightmode">
-                          {item.symbol}
-                        </div>
+            <div class="fs-item-container">
+              {#each dashboard.tokens as item}
+                {#if item.type > 0}
+                  <div
+                    class:checked={item.checked}
+                    class:fs-dashboard-item__origin={name === "Origin"}
+                    class="loanitem fs-dashboard-item  fs-loan-item"
+                    on:click={() => name == "Origin" && setChecked(item.symbol, !item.checked)}
+                    value={item.symbol}
+                    checked={item.checked}
+                  >
+                    <div class="tokendetails reverse">
+                      <div id="platformAddressLogo" class="buttondisk reverse">
+                        <img
+                          src="https://d3e54v103j8qbb.cloudfront.net/plugins/Basic/assets/placeholder.60f9b1840c.svg"
+                          loading="lazy"
+                          id="tokenLogoLoan01ORG"
+                          alt=""
+                          class="placeholderimage"
+                        />
                       </div>
-                      <div class="readonlyfield">
-                        <div id="amountLoan01ORG" class="textlightmode numbers">
-                          {_bal(item.amount, item.decimals)}
-                        </div>
+                      <div id="tokenSymbolLoan01" class="textlightmode">
+                        {item.symbol}
                       </div>
-                      {#if isOrigin}
-                        <div class="fs-checkmark">
-                          {#if item.checked}V{:else}O{/if}
-                        </div>
-                      {/if}
                     </div>
-                    <div
-                      id="APRLoan01ORG"
-                      class="ratesinfo w-node-9c5920cd5a3d-3e5b97ee"
-                    >
-                      <div id="tokenSymbolDep01ORG" class="textlightmode rates">
-                        {item.type == 2 ? "Variable rate" : "Stable rate"}
+                    <div class="readonlyfield">
+                      <div id="amountLoan01ORG" class="textlightmode numbers">
+                        {_bal(item.amount, item.decimals)}
                       </div>
-                      <!-- <img
+                    </div>
+                    {#if name == "Origin"}
+                      <div class="fs-checkmark">
+                        {#if item.checked}V{:else}O{/if}
+                      </div>
+                    {/if}
+                  </div>
+                  <div id="APRLoan01ORG" class="ratesinfo w-node-9c5920cd5a3d-3e5b97ee">
+                    <div id="tokenSymbolDep01ORG" class="textlightmode rates">
+                      {item.type == 2 ? "Variable rate" : "Stable rate"}
+                    </div>
+                    <!-- <img
                         src="images/Info-Icon.svg"
                         loading="lazy"
                         alt=""
                         class="infroicon"
                       /> -->
-                    </div>
-                  {/if}
-                {/each}
-              </div>
-            {:else}
-              <div>No positions</div>
-            {/if}
-          </div>
-          <div id="healthFactorInfoORG" class="healthfactorinfo">
-            <div class="hfcontents origin">
-              <p class="textlightmode rates">
-                Health Factor : {_healthFactor(dashboard)}
-              </p>
+                  </div>
+                {/if}
+              {/each}
             </div>
+          {:else}
+            <div>No positions</div>
+          {/if}
+        </div>
+        <div id="healthFactorInfoORG" class="healthfactorinfo">
+          <div class="hfcontents origin">
+            <p class="textlightmode rates">
+              Health Factor : {_hf(healthFactorAll, 18)}
+            </p>
           </div>
-        {/if}
-      {:catch error}
-        <p style="color: red">{error.message}</p>
-      {/await}
-      <!-- <div id="clearALL" class="secondarybutton">
+        </div>
+      {/if}
+    {:catch error}
+      <p style="color: red">{error.message}</p>
+    {/await}
+    <!-- <div id="clearALL" class="secondarybutton">
         <div on:click={refresh} id="refreshFlashPos" class="textlightmode button">Refresh Dashboard</div>
       </div> -->
-    </div>
-  {/key}
+  </div>
+  <!-- {/key} -->
 </main>
 
 <style>
