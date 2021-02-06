f1adf59dd847d2aa115dd923b9b59def541ab9e3
Alain Papazoglou <alain@kredeum.com>
2021-02-06 16:25:55 +0100

    add message2

--- svelte/flashpos.svelte
+++ svelte/flashpos.svelte
@@ -20,6 +20,7 @@
   let startMigration = false;
   let step = 0;
   let message;
+  let message2;
   let refresh = 0;
 
   // NETWORK MUST BE KOVAN
@@ -29,25 +30,21 @@
 
   // FIRST ADDRESS IS ALICE, SECOND ADDRESS BOB
   $: {
-    if (address && address != Alice && address != Bob && step <= 4)
+    if (address && (address != Alice) && (address != Bob) && (step <= 4))
       if (!Alice) {
         Alice = address;
       } else {
-        Bob = address;
+        if (step == 4){
+          Bob = address;
+        }
       }
-    console.log("ADDRESSES", address);
+    console.log("ADDRESS", address, Alice, Bob, step);
   }
 
   // BALANCE TO LOW
   $: if (address && network == "kovan" && balance == 0) {
     alert("ETH balance is to low to proceed, you need some ETH to pay gas");
   }
-
-  $: console.log("STEP:", step);
-  $: console.log("DASHBOARDS F", $Dashboards);
-  $: console.log("ALICE:", Alice);
-  $: console.log("BOB:", Bob);
-  $: console.log("ADDRESS F", address);
 
   // STEP 0 : initial state
   $: if (Alice && step == 0) step1();
@@ -57,7 +54,7 @@
   // Click button
   // STEP 3 : start migration
   // Transfers approved
-  // STEP 4 : connect destinator
+  // STEP 4 : connect destination
   $: if (Bob && step == 4) step5();
   // STEP 5 : address Bob defined
   $: if (Bob && $Dashboards[Bob] && step == 5) step6();
@@ -86,21 +83,30 @@
   }
   async function step1() {
     step = 1;
-    message = `<<< Origin account connected, retreiving AAVE dashboard...`;
+    message = ">>> Origin account connected";
+    message2 = `<<< Retreiving AAVE dashboard...`;
     startMigration = false;
     if (Alice && $Dashboards[Alice]) step2();
   }
   async function step2() {
     step = 2;
     message = ">>> Select what positions to migrate, and start migration";
+    message2 = "<<< Origin dashboard retreived";
     startMigration = true;
   }
   async function step3() {
-    console.log("STEP3", address, Alice);
+    refresh++;
+    
+
+    console.log("STEP3 AVANT", address, Alice);
     if (address != Alice) {
-      message = "<<< Connect first our browser wallet with origin account !";
-      step2();
+      $Dashboards[Alice]=null;
+      Alice="";
+      message2 = "<<< Keep your browser wallet connected with same origin account !";
+      setTimeout(step0, 2000);
+      return;
     }
+    console.log("STEP3 APRES", address, Alice);
     step = 3;
     startMigration = false;
     positionsAlice = $Dashboards[Alice].tokens.filter((pos) => pos.checked);
@@ -119,25 +125,28 @@
         let ia = 0;
         for (const deposit of deposits) {
           const amount = `${_bal(deposit.amount, deposit.decimals)} ${deposit.symbol}`;
-          message = `>>> Approve the transfer of your ${nd} deposits with your browser wallet`;
+          message = `>>> Approve the transfer of your ${nd} deposit${nd > 1 ? "s" : ""} with your browser wallet`;
           txsDeposit[ic] = FlashAccountsContract.approveTransfer(deposit, signer);
           amounts[ic] = amount;
           ic++;
         }
+        message = `>>> You did approve ${nd} deposit${nd > 1 ? "s" : ""}`;
+        message2 = `<<< ${nd} requests sent`;
         for await (const txDeposit of txsDeposit) {
           console.log(`TX1.${iw + 1}/${nd} CALL`, txDeposit);
           txsWait[iw] = txDeposit.wait();
           iw++;
         }
         for await (const tx of txsWait) {
-          message = `<<< Waiting approvals... ${ia + 1}/${nd} deposit${ia > 0 ? "s" : ""} approved`;
-          console.log(`TX1.${ia + 1}/${nd} END`, tx);
+          message2 = `<<< Waiting requests completion... ${ia + 1}/${nd} deposit${ia > 1 ? "s" : ""} completed`;
+          console.log(`TX1.${ia+1}/${nd} END`, tx);
           ia++;
         }
+        message2 = `<<< ${nd > 1 ? "All " + nd + " deposits" : "Deposit" } request${nd > 1 ? "s" : ""} completed`;
       }
       step4();
     } catch (e) {
-      message = "<<< Transaction failed";
+      message2 = "<<< Transaction failed";
       console.error(e);
     }
   }
@@ -149,11 +158,14 @@
   async function step5() {
     console.log("Bob", Bob);
     step = 5;
-    message = "<<< Destinator account connected, retreiving AAVE dashboard...";
+    message = ">>> Destination account connected";
+    message2 = "<<< Retreiving destination dashboard...";
     if (Bob && $Dashboards[Bob]) step6();
   }
   async function step6() {
     step = 6;
+    message2 = "<<< Destination dashboard retreived";
+
     const loans = positionsAlice.filter((pos) => pos.type != 0);
     const nl = loans.length;
     try {
@@ -166,25 +178,28 @@
         let il = 0;
         for (const loan of loans) {
           const amount = `${_bal(loan.amount, loan.decimals)} ${loan.symbol}`;
-          message = `>>> Approve the credit delegation of your ${nl} loans with your browser wallet`;
+          message = `>>> Approve the credit delegation of your ${nl} loan${nl > 1 ? "s" : ""} with your browser wallet`;
           txsLoan[ic] = await FlashAccountsContract.approveLoan(loan, signer);
           amounts[ic] = amount;
           ic++;
         }
+        message = `>>> You did approve ${nl} loan${nl > 1 ? "s" : ""}`;
+        message2 = `<<< ${nl} requests sent`;
         for await (const txLoan of txsLoan) {
           console.log(`TX2.${iw + 1}/${nl} CALL`, txLoan);
           txsWait[iw] = txLoan.wait();
           iw++;
         }
         for await (const tx of txsWait) {
-          message = `<<< Waiting approvals... ${il + 1}/${nl} loan${il > 0 ? "s" : ""} approved`;
-          console.log(`TX2.${il + 1}/${nl} END`, tx);
+          message2 = `<<< Waiting requests completion... ${il + 1}/${nl} loan${il > 1 ? "s" : ""} completed`;
+          console.log(`TX2.${il+1}/${nl} END`, tx);
           il++;
         }
+        message2 = `<<< ${nl > 1 ? "All " + nl + " loans" : "Loan" } request${nl > 1 ? "s" : ""} completed`;
       }
       step7();
     } catch (e) {
-      message = "<<< Transaction failed";
+      message2 = "<<< Transaction failed";
       console.error(e);
     }
   }
@@ -194,22 +209,25 @@
     try {
       const tx = await FlashAccountsContract.callFlashLoanTx(positionsAlice, Alice, Bob, signer);
 
-      message = `<<< Flash Loan Magic in progress... wait a few seconds`;
+      message2 = `<<< Flash Loan Magic in progress... wait a few seconds`;
       console.log(`TX2`, await tx.wait());
       step8();
     } catch (e) {
-      message = "<<< Transaction failed";
+      message2 = "<<< Transaction failed";
       console.error(e);
     }
   }
   async function step8() {
     step = 8;
-    message = "<<< Flash Loan succeeded !  refreshing dashboards";
+    message = ">>> Flash Loan succeeded !";
+    message2 = "<<< Refreshing dashboards";
     refresh++;
+    setTimeout(step0, 10000);
   }
   async function step9() {
     step = 9;
-    message = "<<< Account migrated !";
+    message = ">>> Refresh your browser to start another migration";
+    message2 = "";
   }
 </script>
 
@@ -233,6 +251,7 @@
       <h1>Migrate your positions</h1>
 
       <p>{message}</p>
+      <p>{message2}</p>
 
       <div class="columnspositions fs-columnspositions w-row">
         <div id="chipFlashPos" class="sectionchip fs-chip">
